7a55cd6972f062a738ba9c0d181b2ee29c7b87da
Никита Ефремов <vomerff@gmail.com>
2023-10-11 14:56:37 +0700

    correct some issues

--- ant_path.py
+++ ant_path.py
@@ -2,46 +2,34 @@ import numba as nb
 import numpy as np
 from PIL import Image
 
+
 @nb.njit
 def simulate(field, ant_x, ant_y, direction):
     height, width = field.shape
-    
     while 0 <= ant_x < width and 0 <= ant_y < height:
-        
         current_value = field[ant_y, ant_x]
-        
         if current_value == 1:
             direction = (direction + 1) % 4
         else:
             direction = (direction - 1) % 4
-            
         field[ant_y, ant_x] = 1 - current_value
-        
         if direction == 0:
-            ant_y += 1  # Перемещение вниз
+            ant_y += 1
         elif direction == 1:
-            ant_x += 1  # Перемещение вправо
+            ant_x += 1
         elif direction == 2:
-            ant_y -= 1  # Перемещение вверх
+            ant_y -= 1
         elif direction == 3:
-            ant_x -= 1  # Перемещение влево
-
+            ant_x -= 1
     return field
 
-# Инициализация 
+
 width, height = 1024, 1024
 field = np.ones((width, height), np.uint8)
-ant_x, ant_y = 512, 512 
+ant_x, ant_y = 512, 512
 direction = 0
-
-# Симуляция
 field = simulate(field, ant_x, ant_y, direction)
-
-# Подсчет черных клеток
 black_cells = np.count_nonzero(field == 0)
-
-# Сохранение результата
 img = Image.fromarray(field * 255)
 img.save("ant_field.png")
-
 print(f"Черных клеток: {black_cells}")
